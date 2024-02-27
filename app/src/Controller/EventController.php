34413cd376b26c8e1692304771c179e799ca2ee5
Alexis Mathiot <alexis.mathiot@gmail.com>
2024-02-27 12:16:51 +0100

    add test for create and delete user

--- app/src/Controller/EventController.php
+++ app/src/Controller/EventController.php
@@ -173,8 +173,7 @@ class EventController extends AbstractController
 
     }
 
-    #[
-        Route('api/events/organizer/{userId}/{eventId}', name: 'set_organizer', methods: ['GET'])]
+    #[Route('api/events/organizer/{userId}/{eventId}', name: 'set_organizer', methods: ['GET'])]
     public function setOrganizer(int                    $userId,
                                  int                    $eventId,
                                  UserRepository         $userRepository,
